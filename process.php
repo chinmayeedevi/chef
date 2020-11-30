6bd737498653643d6faaedf81731bc2b53d5933c
imvishalnag <imvishalnag@gmail.com>
2020-11-30 18:15:07 +0530

    update

---
+++ process.php
@@ -0,0 +1,87 @@
+<?php include "include/header.php" ?>
+
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Start Page Title Area
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <div class="page-title-area bg-image bg-parallax2"
+            style="background-image:url('assets/images/header/header1.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-header-content text-center">
+                            <div class="page-header-caption">
+                                <h2 class="page-title">Process</h2>
+                            </div>
+                            <div class="breadcrumb-area">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                    <li class="breadcrumb-item active">Process</li>
+                                </ol>
+                            </div>
+                            <!--~~./ breadcrumb-area ~~-->
+                        </div>
+                        <!--~~./ page-header-content ~~-->
+                    </div>
+                </div>
+            </div>
+            <!--~~./ end container ~~-->
+        </div>
+        <!--~~./ end page title area ~~-->
+
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Start About Us Block
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <div class="about-us-block pd-t-100">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="process-block">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 1</h3>
+                                        <h5>Project Planning & Design</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 2</h3>
+                                        <h5>Systems Planning</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 3A</h3>
+                                        <h5>Technical Planning (A)</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 3B</h3>
+                                        <h5>Technical Planning (B)</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 4</h3>
+                                        <h5>Operations Planning <br> Control System</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="step">
+                                        <h3>Step 5</h3>
+                                        <h5>Pre-opening Services & Planning</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div>
+        <!--~~./ end about us block ~~-->
+        
+        <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Start SITE FOOTER
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+        <?php include "include/footer.php" ?>
